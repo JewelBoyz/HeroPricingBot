0a996758d58370913436420ba9ce640db88b2af3
Wellington Johnson <ajistrying@gmail.com>
2021-12-06 20:54:04 -0500

    Better error handling and start of new query to start pulling comparable heros

--- src/main.rs
+++ src/main.rs
@@ -52,24 +52,54 @@ struct HeroPricing;
 async fn hpb(ctx: &Context, msg: &Message) -> CommandResult{
 
     // TODO: Need to better handle errors to send in discord
-    let user_input = msg.content[4..].trim().parse::<i64>().ok().expect("Enter a valid ID");
+    let user_input = msg.content[4..].trim().parse::<i64>();
+
+    // if the Result struct turns out to be an error, we'll tell the user to enter in a valid argument: a number
+    if user_input.is_err() {
+        msg.reply(ctx, format!("Please enter a valid hero id")).await?;
+        return Ok(());
+    };
+
+    // At this point we know the user's input is i64, so we can grab the Option out of the Result, 
+    //and then unwrap it, but it something wild happens we provide a default of 0
+    let dfk_hero_id = user_input.ok().unwrap_or(0);
+
+    // One more check to see if the now validated number entered is >= 0, otherwise show another error
+    if dfk_hero_id < 0 {
+        msg.reply(ctx, format!("Please enter a valid hero id")).await?;
+        return Ok(());
+    };
 
     // The shape of the variables expected by the query.
-    let variables = dfk_query::Variables {
-        id: user_input
+    let variables = initial_dfk_hero_query::Variables {
+        id: dfk_hero_id
     };
 
     // Produce a GraphQL query struct that can be JSON serialized and sent to a GraphQL API
-    let request_body = DFKQuery::build_query(variables);
+    let request_body = InitialDFKHeroQuery::build_query(variables);
 
     // Create a new client to send the request body to the graphql server
     let client = reqwest::Client::new();
-    // Send the request and (hopefully) get a response back
-    let res = client.post("https://graph4.defikingdoms.com/subgraphs/name/defikingdoms/apiv5").json(&request_body).send().await?;
-    // Parse the response into json
-    let response_body: Response<dfk_query::ResponseData> = res.json().await?;
 
-    let response_data = response_body.data.expect("missing response data");
+    // Send the request with the request body being the variables we send in parsed from the user input, 
+    //we (hopefully) get a response back and store it
+    let res = client.post("https://graph4.defikingdoms.com/subgraphs/name/defikingdoms/apiv5").json(&request_body).send().await?;
+
+    // let's test that we didn't get a nasty server error from the graphql api
+    if res.status().is_server_error() {
+        msg.reply(ctx, format!("Received a server error from the api")).await?;
+        return Ok(());
+    };
+
+    // Parse the response into json and pop it into this custom ResponseData struct
+    let response_body: Response<initial_dfk_hero_query::ResponseData> = res.json().await?;
+
+    // 
+    if response_body.data.is_none() {
+        msg.reply(ctx, format!("That hero does not exist")).await?;
+        return Ok(());
+    };
+    let response_data = response_body.data.unwrap();
 
     // First, lets pull stuff from the response_data
     let hero_obj = response_data.hero;
@@ -83,6 +113,7 @@ async fn hpb(ctx: &Context, msg: &Message) -> CommandResult{
                 profession: hero_obj.profession,
                 level: hero_obj.level,
             };
+            let result = find_comparable_heroes(hero);
             msg.reply(ctx, format!("{:?}", hero)).await?;
         },
         None => {
@@ -103,7 +134,16 @@ async fn hpb(ctx: &Context, msg: &Message) -> CommandResult{
     query_path = "src/query.graphql",
     response_derives = "Debug, Serialize, Deserialize"
 )]
-pub struct DFKQuery;
+pub struct InitialDFKHeroQuery;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/schema.graphql",
+    query_path = "src/comparisonQuery",
+    response_derives = "Debug, Serialize, Deserialize"
+)]
+pub struct ComparableDFKHerosQuery;
+
 
 #[derive(Debug)]
 pub struct Hero {
@@ -114,3 +154,7 @@ pub struct Hero {
     level: i64
 }
 
+// ***** Custom Functionality ***** //
+
+// Take hero response data and pipe into a new query that will:
+// 1. Search for heros hero according to similar: class, rarity, profession, number of summons, generation
